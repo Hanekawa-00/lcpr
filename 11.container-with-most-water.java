99692e6b8e64def723dd18e94557ea946c7897e8
zzh <zzh04004@gmail.com>
2024-10-09 09:29:15 +0800

    lcpr

---
+++ 11.container-with-most-water.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=11 lang=java
+ * @lcpr version=30204
+ *
+ * [11] 盛最多水的容器
+ */
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+    public int maxArea(int[] height) {
+        int l = 0;
+        int r = height.length - 1;
+        int res = 0;
+        while (l < r) {
+            int area = Math.min(height[l], height[r]) * (r - l);
+            res = Math.max(area, res);
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
+/*
+ * // @lcpr case=start
+ * // [1,8,6,2,5,4,8,3,7]\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // [1,1]\n
+ * // @lcpr case=end
+ * 
+ */
