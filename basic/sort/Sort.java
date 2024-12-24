02166ff9d65bb2fb8bb67b7daddf91ea0affff99
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2024-12-24 12:55:56 +0000

    hello

--- basic/sort/Sort.java
+++ basic/sort/Sort.java
@@ -1,30 +1,66 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import java.util.stream.Collectors;
+
 public class Sort {
+    /**
+     * 插入排序
+     * 根本就是使得有序区间扩大（一个一个元素添加）
+     * 
+     * @param nums
+     */
     private static void insertionSort(int[] nums) {
         for (int i = 1; i < nums.length; i++) {
-            int base = nums[i];// 目标
-            int j = i - 1;
-            while (j >= 0 && nums[j] >= base) {
-                nums[j + 1] = nums[j];// 记录需要插入的位置的数
+            // base 用来暂时存储第i个值（temp），j是i的前一个索引即已经排序区间的右端
+            int base = nums[i], j = i - 1;
+            // 当已经排序好的区间中的最右边的值（最大值）大于base时一直寻找插入点，直到找到小于等于或者到区间最左边停止
+            while (nums[j] > base && j >= 0) {
+                // 在寻找插入点时顺便让区间中的数向右移动
+                nums[j + 1] = nums[j];
                 j--;
             }
+            // 在插入点将base插入
             nums[j + 1] = base;
         }
     }
 
+    /**
+     * 冒泡排序
+     * <p>
+     * 选择一个大区间最右边的作为最大值，
+     * 再对这个区间作遍历，相邻的两个数大的左边的大则交换右移。
+     * 与选择排序不同的是，选择排序是直接依次满足条件就交换。
+     * 冒泡就相当于区间内部动态交换数据
+     * </p>
+     * 
+     * @param nums
+     */
     private static void bubbleSort(int[] nums) {
         for (int i = nums.length - 1; i > 0; i--) {
-            // 内循环：将未排序区间 [0, i] 中的最大元素交换至该区间的最右端
+            boolean flag = false;
             for (int j = 0; j < i; j++) {
                 if (nums[j] > nums[j + 1]) {
-                    // 交换 nums[j] 与 nums[j + 1]
-                    int tmp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = tmp;
+                    swap(nums, j + 1, j);
+                    flag = true;
                 }
+            }
+            if (!flag) {
+                break;
             }
         }
     }
 
+    /**
+     * 选择排序
+     * <p>
+     * 根据排序规则可知最左端为最小，最右端是最大。
+     * 所以”选择“一个大区间的最左或最右边的来当作区间的最大最小值。
+     * 遍历最小值右边的子区间或者最大值左边的子区间与当前的最大最小值（bushi）比较交换
+     * </p>
+     * 
+     * @param nums
+     */
     private static void selectionSort(int[] nums) {
         int length = nums.length;
         for (int i = 0; i < length - 1; i++) {
@@ -35,7 +71,7 @@ public class Sort {
                     k = j;// 更新索引
                 }
             }
-            swap(nums, i, k);//将最小元素和区间最左元素交换
+            swap(nums, i, k);// 将最小元素和区间最左元素交换
         }
     }
 
@@ -46,17 +82,17 @@ public class Sort {
      * @param i
      * @param j
      */
-    private static void swap(int[] arr, int i, int j) {
-        arr[i] = arr[i] ^ arr[j];
-        arr[j] = arr[i] ^ arr[j];
-        arr[i] = arr[i] ^ arr[j];
+    private static void swap(int[] nums, int i, int k) {
+        int temp = nums[i];
+        nums[i] = nums[k];
+        nums[k] = temp;
     }
 
     public static void main(String[] args) {
         int[] arr = { 4, 10001, 5, 6, 2, 9, 96, 1000, 5 };
-        bubbleSort(arr);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
+        insertionSort(arr);
+        ArrayList<Integer> list = new ArrayList<>(
+                Arrays.stream(arr).boxed().collect(Collectors.toList()));
+        System.out.println(list);
     }
 }
