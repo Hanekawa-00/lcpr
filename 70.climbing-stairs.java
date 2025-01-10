1f81fa6cce9ecd1f6627e7197ac2d6aaf1a11973
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-01-10 22:53:31 +0800

    爬楼梯

---
+++ 70.climbing-stairs.java
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=java
+ * @lcpr version=30204
+ *
+ * [70] 爬楼梯
+ */
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+    /**
+     * 斐波那契
+     * 
+     * @param n
+     * @return
+     */
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n - 1];
+    }
+}
+// @lc code=end
+
+/*
+ * // @lcpr case=start
+ * // 2\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // 3\n
+ * // @lcpr case=end
+ * 
+ */
