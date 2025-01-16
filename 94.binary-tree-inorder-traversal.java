2545cbc1d5773a2cb2cc11b0b76180e431865e3d
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-01-16 23:07:49 +0800

    中序遍历

---
+++ 94.binary-tree-inorder-traversal.java
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=java
+ * @lcpr version=30204
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        inorderTraversal(res, root);
+        return res;
+    }
+
+    private void inorderTraversal(List<Integer> list, TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inorderTraversal(list, root.left);
+        list.add(root.val);
+        inorderTraversal(list, root.right);
+    }
+}
+// @lc code=end
+
+/*
+ * // @lcpr case=start
+ * // [1,null,2,3]\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // []\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // [1]\n
+ * // @lcpr case=end
+ * 
+ */
