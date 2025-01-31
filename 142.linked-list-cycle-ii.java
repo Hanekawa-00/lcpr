36cff6440e3230fda99fa8302f9c3233c2219e5a
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-01-31 21:13:40 +0800

    环形链表2

---
+++ 142.linked-list-cycle-ii.java
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=142 lang=java
+ * @lcpr version=30204
+ *
+ * [142] 环形链表 II
+ */
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        Set<ListNode> cachedSet = new HashSet<ListNode>();
+        while (head.next != null) {
+            if (cachedSet.contains(head.next)) {
+                return head.next;
+            }
+            cachedSet.add(head);
+            head = head.next;
+        }
+        return null;
+    }
+}
+// @lc code=end
+
+/*
+ * // @lcpr case=start
+ * // [3,2,0,-4]\n1\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // [1,2]\n0\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // [1]\n-1\n
+ * // @lcpr case=end
+ * 
+ */
