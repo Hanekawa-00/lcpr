a761a62483bdbf11cb9270b54ca6bec68c336947
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-02-08 20:17:42 +0800

    轮转数组

---
+++ 189.rotate-array.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=189 lang=java
+ * @lcpr version=30204
+ *
+ * [189] 轮转数组
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        int[] newArr = new int[n];
+        for (int i = 0; i < n; ++i) {
+            newArr[(i + k) % n] = nums[i];
+        }
+        System.arraycopy(newArr, 0, nums, 0, n);
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3,4,5,6,7]\n3\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [-1,-100,3,99]\n2\n
+// @lcpr case=end
+
+ */
+
