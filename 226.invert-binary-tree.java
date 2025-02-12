28911a68f50b40cb306c16949477405ff925fcde
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-02-12 19:50:44 +0800

    翻转二叉树

---
+++ 226.invert-binary-tree.java
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=java
+ * @lcpr version=30204
+ *
+ * [226] 翻转二叉树
+ */
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.left = invertTree(right);
+        root.right = invertTree(left);
+        return root;
+    }
+}
+// @lc code=end
+
+/*
+ * // @lcpr case=start
+ * // [4,2,7,1,3,6,9]\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // [2,1,3]\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // []\n
+ * // @lcpr case=end
+ * 
+ */
