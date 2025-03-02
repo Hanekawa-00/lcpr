55f5beee5e39ea5dc4ba36d30e7ea87ccfb7d640
zzh <zzh04004@gmail.com>
2025-03-02 02:43:55 +0000

    反转链表

---
+++ LCR 141.训练计划-iii.java
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=LCR 141 lang=java
+ * @lcpr version=30204
+ *
+ * [LCR 141] 训练计划 III
+ */
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode trainningPlan(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode pre = null;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = pre;
+            pre = head;
+            head = temp;
+        }
+        return pre;
+    }
+}
+// @lc code=end
+
+/*
+ * // @lcpr case=start
+ * // [1,2,3,4,5]\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // [1,2]\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // []\n
+ * // @lcpr case=end
+ * 
+ */
