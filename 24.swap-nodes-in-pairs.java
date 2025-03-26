49598ad0063c0fe6feb335ac767cebec8f589214
zzh <zzh04004@gmail.com>
2025-03-26 04:12:55 +0000

    两两交换链表中的节点

--- 24.swap-nodes-in-pairs.java
+++ 24.swap-nodes-in-pairs.java
@@ -21,22 +21,24 @@
  */
 class Solution {
     public ListNode swapPairs(ListNode head) {
-        if (head == null) {
-            return null;
+        // 创建哑结点，防止第一个节点的特殊判断
+        ListNode dummpyHead = new ListNode(-1);
+        dummpyHead.next = head;
+        ListNode curr = dummpyHead;
+        // 目的是交换curr后面的两个节点,curr在这里相当于前置节点
+        while (curr.next != null && curr.next.next != null) {
+            ListNode node1 = curr.next;
+            ListNode node2 = node1.next;
+            // 获取这两个节点后面的那个节点
+            ListNode nextNode = node2.next;
+            // 交换
+            node2.next = node1;
+            node1.next = nextNode;
+            curr.next = node2;
+            // 更新
+            curr = node1;
         }
-        if (head.next == null) {
-            return head;
-        }
-        head = swap(head);
-        head.next.next = swapPairs(head.next.next);
-        return head;
-    }
-
-    private ListNode swap(ListNode node) {
-        ListNode temp = node.next;
-        node.next = node.next.next;
-        temp.next = node;
-        return temp;
+        return dummpyHead.next;
     }
 }
 // @lc code=end
