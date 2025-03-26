d44802bcfb5fc92a04e1ee28e2a2c0703c673f76
zzh <zzh04004@gmail.com>
2025-03-26 02:21:22 +0000

    有效括弧

--- 20.valid-parentheses.java
+++ 20.valid-parentheses.java
@@ -14,6 +14,18 @@ import java.util.Stack;
 
 class Solution {
     public boolean isValid(String s) {
+        /*
+         * 想象一下我们手动检查括号是否匹配的过程。当我们遇到一个左括号 (, {, [ 时，我们期望在后面的某个位置遇到一个对应的右括号 ), }, ]
+         * 来与之匹配。
+         * 
+         * 关键在于“正确顺序”。比如 ([)] 就不是有效的，因为 ( 先出现，然后是 [，但 ] 却在 ) 之前闭合了 [，导致 ( 无法与 )
+         * 匹配。这暗示了后出现的左括号需要先被闭合。
+         * 
+         * 这 “后进先出”（Last-In, First-Out, LIFO）的特性是不是让你想到了什么数据结构？
+         * 
+         * 没错！就是 栈（Stack）。
+         * 
+         */
         Stack<Character> stack = new Stack<>();
         for (char ch : s.toCharArray()) {
             if (ch == '('
