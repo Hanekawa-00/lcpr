aa6a7cfb601ed4728a1faf97d2d7dd0840c3b96e
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-03-29 12:31:11 +0800

    搜索旋转排序数组

--- 33.search-in-rotated-sorted-array.java
+++ 33.search-in-rotated-sorted-array.java
@@ -11,40 +11,40 @@
 // @lc code=start
 
 class Solution {
+    /**
+     * 虽然是在非正常的有序数组中二分查找
+     * 但是只要发现有序区间就非常好处理
+     * 总体思路就是：
+     * 1.先找到有序区间
+     * 2.判断是否在这个区间
+     * 3.是则缩小查找区间到这个子区间
+     * 4.否则继续在这个子区间之外的另一个区间内寻找
+     * 
+     * @param nums
+     * @param target
+     * @return
+     */
     public int search(int[] nums, int target) {
-        int n = nums.length;
-        if (n == 0) {
-            return -1;
-        }
-        if (n == 1) {
-            return nums[0] == target ? 0 : -1;
-        }
-        int l = 0, r = n - 1;
-        // 使用循环进行二分，每次缩小区间
-        // 根据有序的部分来判断目标值是否在有序的那部分里。如果在，就缩小搜索范围到有序的那部分；否则，就去另一部分继续搜索。(相当于在子数组中继续进行查找)
-        // 为什么每次都选择有序区间进行边界判断？假如每次都与mid进行比较的话，因为假如根据mid和target的比较选择了一个无序区间，很可能会错过target
-        while (l <= r) {
-            int mid = (l + r) / 2;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
             if (nums[mid] == target) {
                 return mid;
             }
-            // 二分数组，总有一边的区间是有序区间
-            // 如果[0,mid]（左半部分）是有序区间
-            if (nums[0] <= nums[mid]) {
-                if (nums[0] <= target && target < nums[mid]) {
-                    // target在有序区间[0,mid]内
-                    r = mid - 1;
+            // 如果这里写成 nums[mid] > nums[left]，在 left == mid 的情况下会判断错误。用 >= 包含了 left == mid
+            // 的情况，确保了当 mid 就是 left 时，[left, mid] 这段（实际上只有一个元素）被认为是“有序”的。
+            if (nums[mid] >= nums[left]) {
+                if (target < nums[mid] && target >= nums[left]) {
+                    right = mid - 1;
                 } else {
-                    // target在区间[mid,n-1]内
-                    l = mid + 1;
+                    left = mid + 1;
                 }
-            } else {// 如果[mid,n-1]（右半部分）是有序区间
-                if (nums[mid] < target && target <= nums[n - 1]) {
-                    // 如果target在有序区间[mid,n-1]内，l右移缩小区间
-                    l = mid + 1;
+            } else {
+                if (target <= nums[right] && target > nums[mid]) {
+                    left = mid + 1;
                 } else {
-                    // 如果target在区间[0,mid]
-                    r = mid - 1;
+                    right = mid - 1;
                 }
             }
         }
