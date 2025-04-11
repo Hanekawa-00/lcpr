9d6bf1d34c8428e60a4f5dadd5958b0291d74e2b
zzh <zzh04004@gmail.com>
2025-04-11 07:01:39 +0000

    接雨水

--- 42.trapping-rain-water.java
+++ 42.trapping-rain-water.java
@@ -11,52 +11,32 @@
 // @lc code=start
 class Solution {
     /**
-     * 每个柱子上面的雨水格数=min(左边最高柱子，右边最高柱子)-当前柱子高度 （例外情况：结果为负数，判断为0）
-     * 
-     * @param height
-     * @return
-     */
-    public int trap1(int[] height) {
-        // 中间数组，用来存储每一个柱子所对应的左右最高柱子
-        int[] lmax = new int[height.length];
-        int[] rmax = new int[height.length];
-        lmax[0] = 0;
-        rmax[height.length - 1] = 0;
-        for (int i = 1; i < height.length; i++) {
-            lmax[i] = Math.max(lmax[i - 1], height[i - 1]);
-        }
-        for (int j = height.length - 2; j >= 0; j--) {
-            rmax[j] = Math.max(rmax[j + 1], height[j + 1]);
-        }
-        int res = 0;
-        for (int i = 1; i < height.length - 1; i++) {
-            res += Math.max(0, Math.min(lmax[i], rmax[i]) - height[i]);
-        }
-        return res;
-    }
-
-    /**
-     * 双指针版本2
+     * 每个区域的水量公式：min(leftMax,rightMax) - height[i]
      * 
      * @param height
      * @return
      */
     public int trap(int[] height) {
-        // l,r是将要计算雨水格数的索引
-        int l = 1, r = height.length - 2, lmax = height[0], rmax = height[height.length - 1], res = 0;
-        // 双指针启动
-        while (l <= r) {
-            if (lmax <= rmax) {// 此时真实的rmax必然大于等于这个rmax，即lmax必然比真实的rmax小
-                res += Math.max(0, lmax - height[l]);
-                lmax = Math.max(lmax, height[l]);
-                l++;// l处已计算，故++
-            } else {
-                res += Math.max(0, rmax - height[r]);
-                rmax = Math.max(rmax, height[r]);
-                r--;
+        int len = height.length;
+        // 用来维护i左边最高的柱子高度
+        int[] leftMax = new int[len];
+        leftMax[0] = -1;
+        int[] rightMax = new int[len];
+        rightMax[len - 1] = -1;
+        for (int i = 1; i < leftMax.length; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i - 1]);
+        }
+        for (int i = rightMax.length - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i + 1]);
+        }
+        int ans = 0;
+        for (int i = 1; i < rightMax.length - 1; i++) {
+            int size = Math.min(leftMax[i], rightMax[i]) - height[i];
+            if (size > 0) {
+                ans += size;
             }
         }
-        return res;
+        return ans;
     }
 }
 // @lc code=end
