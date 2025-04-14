e4de89d6b41c8bd64e7696c90cab175e711dde3a
zzh <zzh04004@gmail.com>
2025-04-14 06:33:42 +0000

    最大子数组和

--- 53.maximum-subarray.java
+++ 53.maximum-subarray.java
@@ -11,15 +11,20 @@
 // @lc code=start
 class Solution {
     public int maxSubArray(int[] nums) {
-        if (nums.length == 1) {
-            return nums[0];
+        int len = nums.length;
+        // dp[i]用来维护以nums[i]结尾的最大子数组和
+        int[] dp = new int[len];
+        dp[0] = nums[0];
+        int ans = dp[0];
+        for (int i = 1; i < len; i++) {
+            if (dp[i - 1] > 0) {
+                dp[i] = dp[i - 1] + nums[i];
+            } else {
+                dp[i] = nums[i];
+            }
+            ans = Math.max(ans, dp[i]);
         }
-        int pre = 0, res = nums[0];
-        for (int num : nums) {
-            pre = Math.max(num, pre + num);
-            res = Math.max(pre, res);
-        }
-        return res;
+        return ans;
     }
 }
 // @lc code=end
