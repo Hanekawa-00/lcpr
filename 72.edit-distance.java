ff8555cf0042196a9585c2cfaa50c3d1d40462c9
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-04-15 19:12:55 +0800

    转化字符串步数

--- 72.edit-distance.java
+++ 72.edit-distance.java
@@ -13,31 +13,27 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int m = word1.length();
         int n = word2.length();
-        // dp用来维护word1的前i个字符变为word2的前j个字符的编辑距离（步数）
+        // dp[i][j]表示word1前i个字符组成的字符串转化为word2前j各字符串所需要好的最少操作数
         int[][] dp = new int[m + 1][n + 1];
-        // 初始化dp
-        for (int i = 0; i < m + 1; i++) {
-            // 第一列，word2为空串，word1变为word2只能删除
+        // 初始化第一行
+        for (int j = 0; j <= n; j++) {
+            // 空字符串转化为word2前j个字符所需要的最少操作数
+            dp[0][j] = j; // 都是插入操作
+        }
+        // 初始化第一列
+        for (int i = 0; i <= m; i++) {
+            // word1转化为一个空字符串所需要的操作数
             dp[i][0] = i;
         }
-        for (int i = 0; i < n + 1; i++) {
-            // 第一行，word1为空串，word1变为word2只能插入
-            dp[0][i] = i;
-        }
-        for (int i = 1; i < m + 1; i++) {
-            for (int j = 1; j < n + 1; j++) {
-                // 如果word1的第i个字符和word2的第j个字符相同
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
                 if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-                    // 不需要操作
-                    // 为什么是(i - 1)和(j - 1)？因为word1的前i-1个字符变为word2的前j-1个字符的编辑距离已经计算
-                    // 而第i个字符和第j个字符相同，所以不需要操作
                     dp[i][j] = dp[i - 1][j - 1];
                 } else {
-                    // 否则，取三种操作的最小值，+1是因为需要操作一次
-                    // dp[i - 1][j - 1]+1：替换（word1的第i个字符替换为word2的第j个字符）
-                    // dp[i - 1][j]+1：删除
-                    // dp[i][j - 1]+1：插入
-                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                    dp[i][j] = 1 + Math.min(
+                        dp[i][j - 1], // 插入
+                        Math.min(dp[i - 1][j], dp[i - 1][j - 1]) // 删除、替换
+                    );
                 }
             }
         }
