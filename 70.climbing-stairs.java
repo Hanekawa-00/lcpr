cd19bc7a4f59b270d562023980066f7a877fc4c2
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-04-15 19:12:32 +0800

    爬楼梯

--- 70.climbing-stairs.java
+++ 70.climbing-stairs.java
@@ -20,13 +20,17 @@ class Solution {
         if (n == 1) {
             return 1;
         }
-        int[] dp = new int[n];
-        dp[0] = 1;
-        dp[1] = 2;
-        for (int i = 2; i < dp.length; i++) {
+        if (n == 2) {
+            return 2;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i < dp.length; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
-        return dp[n - 1];
+        return dp[n];
     }
 }
 // @lc code=end
