dfa0061e4fa940fb22facd3c6aa5ce85143697bd
zzh <zzh04004@gmail.com>
2025-04-16 06:45:18 +0000

    搜索二维数组

--- 74.search-a-2-d-matrix.java
+++ 74.search-a-2-d-matrix.java
@@ -11,40 +11,43 @@
 // @lc code=start
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        int up = 0, low = rows - 1;
-        while (up < low) {
-            int mid = (up + low) / 2;
-            if (target == matrix[mid][0]) {
+        int t = 0;
+        int d = matrix.length - 1;
+        int targetRow = -1;
+        // 找到目标所在行
+        while (t <= d) {
+            int midRow = (t + d) / 2;
+            if (matrix[midRow][0] == target) {
                 return true;
             }
-            // target 大于中间的区间
-            if (target > matrix[mid][cols - 1]) {
-                up = mid + 1;
-            } else if (target < matrix[mid][0]) {
-                // target小于中间的区间
-                low = mid - 1;
+            if (target > matrix[midRow][0]
+                    && target <= matrix[midRow][matrix[0].length - 1]) {
+                targetRow = midRow;
+                break;
+            }
+            if (target > matrix[midRow][0]) {
+                t = midRow + 1;
             } else {
-                // target位于中间的区间
-                up = mid;
-                low = mid;
+                d = midRow - 1;
             }
         }
-        // 对该行进行二分查找
-        int left = 0, right = matrix[0].length - 1;
+        if (targetRow == -1) {
+            return false;
+        }
+        int left = 0;
+        int right = matrix[0].length - 1;
         while (left < right) {
             int mid = (left + right) / 2;
-            if (target == matrix[up][mid]) {
+            if (matrix[targetRow][mid] == target) {
                 return true;
             }
-            if (target > matrix[up][mid]) {
-                left = mid + 1;
-            } else {
+            if (matrix[targetRow][mid] > target) {
                 right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
-        return matrix[up][left] == target;
+        return matrix[targetRow][left] == target;
     }
 }
 // @lc code=end
