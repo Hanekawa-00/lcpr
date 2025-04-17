28ff5a9a5a7efde6ab5a01c1114f61c5696e8696
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-04-17 21:26:51 +0800

    对称二叉树

--- 101.symmetric-tree.java
+++ 101.symmetric-tree.java
@@ -26,18 +26,22 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-       
+        if (root == null) {
+            return true;
+        }
         return check(root.left, root.right);
     }
 
-    private boolean check(TreeNode left, TreeNode right) {
-        if (left == null && right == null) {
+    private boolean check(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) {
             return true;
         }
-        if (left == null || right == null) {
+        if (node1 == null || node2 == null) {
             return false;
         }
-        return left.val == right.val && check(left.left, right.right) && check(left.right, right.left);
+        return node1.val == node2.val
+                && check(node1.left, node2.right)
+                && check(node1.right, node2.left);
     }
 
 }
