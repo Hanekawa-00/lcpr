5c591f8ef8cc02e32e341c0b4592493b0b6b5aa2
zzh <zzh04004@gmail.com>
2025-04-18 03:08:42 +0000

    从前序遍历和中序遍历的数组创建二叉树

--- 105.construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105.construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -26,49 +26,34 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        int n = preorder.length;
-        return build(preorder, inorder, 0, n - 1, 0, n - 1);
-    }
-
-    /**
-     * 前序遍历的遍历顺序是根节点+左子树+右子树（其中左右子树的遍历方式相同）
-     * 中序遍历的遍历顺序是左子树+根节点+右子树
-     * 由此我们可以知道preStartIndex所在位置是根节点，在根据这个值能够在中序遍历中找到根节点索引从而划分出左右子树的区间
-     * 然后根据这两个区间的大小可以得到在前序遍历中的左右子树所在区间
-     * 由此分别递归左右区间得到左右子树
-     * 
-     * @param preorder      前序遍历元数组
-     * @param inorder       中序遍历元数组
-     * @param preStartIndex 前序遍历的子区间start
-     * @param preEndIndex
-     * @param inStartIndex
-     * @param inEndIndex
-     * @return
-     */
-    private TreeNode build(int[] preorder, int[] inorder, int preStartIndex, int preEndIndex, int inStartIndex,
-            int inEndIndex) {
-        if (preStartIndex > preEndIndex) {
-            return null;
-        }
-        // 根节点是前序遍历（根左右）第一个元素
-        int rootVal = preorder[preStartIndex];
-        TreeNode root = new TreeNode(rootVal);
-        // 计算出左子树区间大小
-        int leftSize = 0;
-        for (int i = inStartIndex; i < inorder.length; i++) {
-            if (inorder[i] == rootVal) {
-                break;
-            }
-            leftSize++;
-        }
-        // 构建左子树
-        root.left = build(preorder, inorder, preStartIndex + 1, preStartIndex + leftSize, inStartIndex,
-                inStartIndex + leftSize);
-        // 构建右子树
-        root.right = build(preorder, inorder, preStartIndex + leftSize + 1, preEndIndex, inStartIndex + leftSize + 1,
-                inEndIndex);
+        TreeNode root = helpBuildTree(preorder, inorder, 0, preorder.length - 1, 0, inorder.length - 1);
         return root;
     }
+
+    private TreeNode helpBuildTree(int[] preorder, int[] inorder, int preI, int preJ, int inI, int inJ) {
+        if (inI > inJ || preI > preJ) {
+            return null;
+        }
+        // 前序遍历的第一个值为rootVal
+        int rootVal = preorder[preI];
+        // 构建根结点
+        TreeNode root = new TreeNode(rootVal);
+        int inRootIndex = inI;
+        int leftTreeCount = 0;
+        for (int i = inI; i <= inJ; i++) {
+            if (inorder[i] == rootVal) {
+                inRootIndex = i;
+                leftTreeCount = i - inI;
+                break;
+            }
+        }
+        TreeNode leftTree = helpBuildTree(preorder, inorder, preI + 1, preI + leftTreeCount, inI, inRootIndex - 1);
+        TreeNode rightTree = helpBuildTree(preorder, inorder, preI + leftTreeCount + 1, preJ, inRootIndex + 1, inJ);
+        root.left = leftTree;
+        root.right = rightTree;
+        return root;
+    }
+
 }
 // @lc code=end
 
