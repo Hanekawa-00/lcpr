b24a5305c55c0ecc04b237451bdd0ab07448d53f
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-04-20 14:23:32 +0800

    单词拆分

--- 139.word-break.java
+++ 139.word-break.java
@@ -10,25 +10,32 @@
 // @lcpr-template-end
 // @lc code=start
 
+import java.util.HashMap;
 import java.util.List;
 
 class Solution {
+    private HashMap<String, Boolean> memo = new HashMap<>();
+
     public boolean wordBreak(String s, List<String> wordDict) {
-        // 定义 dp 数组，dp[i] 表示 s[0:i] 是否可以被拼接,0表示空字符
-        boolean[] dp = new boolean[s.length() + 1];
-        dp[0] = true; // 空字符串可以被拼接
-        // 遍历字符串的每个位置
-        for (int i = 1; i <= s.length(); i++) {
-            // 检查从 j 到 i 的子串是否在字典中
-            for (int j = 0; j < i; j++) {
-                if (dp[j] && wordDict.contains(s.substring(j, i))) {
-                    dp[i] = true;
-                    break; // 找到一种拆分方式即可停止内层循环
+        if (s.length() == 0) {
+            return true;
+        }
+        for (String word : wordDict) {
+            if (s.startsWith(word)) {
+                boolean flag = false;
+                String sub = s.substring(word.length());
+                if (memo.containsKey(sub)) {
+                    flag = memo.get(sub);
+                } else {
+                    flag = wordBreak(sub, wordDict);
+                    memo.put(sub, flag);
+                }
+                if (flag == true) {
+                    return true;
                 }
             }
         }
-        // 返回整个字符串是否可以被拼接
-        return dp[s.length()];
+        return false;
     }
 
 }
