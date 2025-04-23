28432d97c11b8e38cfa3130e3403558fcf3dc6ef
zzh <zzh04004@gmail.com>
2025-04-23 07:04:27 +0000

    乘积最大子数组

--- 152.maximum-product-subarray.java
+++ 152.maximum-product-subarray.java
@@ -10,29 +10,21 @@
 // @lcpr-template-end
 // @lc code=start
 class Solution {
-    /**
-     * 总的思路就是，在一边遍历数组的过程中一边更新max
-     * 就是拿当前最大值和max作比较，取最大
-     * 细节是数组中存在负数，在遍历相乘的过程中当前最大值会变成当前最小值，最小值则相反
-     * 这时候就需要一个imin来维护当前最小值
-     * 
-     * @param nums
-     * @return
-     */
+
     public int maxProduct(int[] nums) {
-        int max = Integer.MIN_VALUE, imax = 1, imin = 1;
-        for (int i = 0; i < nums.length; i++) {
-            // 如果当前元素是小于零的，则交换当前最大值和当前最小值
-            if (nums[i] < 0) {
-                int tmp = imax;
-                imax = imin;
-                imin = tmp;
-            }
-            imax = Math.max(imax * nums[i], nums[i]);
-            imin = Math.min(imin * nums[i], nums[i]);
-            max = Math.max(max, imax);
+        // dpMax[i]维护包含nums第i个元素（以nums[i]结尾）最大子数组乘积
+        int[] dpMax = new int[nums.length];
+        int[] dpMin = new int[nums.length];
+        dpMax[0] = nums[0];
+        dpMin[0] = nums[0];
+        int res = dpMax[0];
+        for (int i = 1; i < nums.length; i++) {
+            // 因为nums[i] 可能是负数，乘上一个最小值就成为了最大值，故作比较（有点无脑）
+            dpMax[i] = Math.max(nums[i], Math.max(nums[i] * dpMax[i - 1], nums[i] * dpMin[i - 1]));
+            dpMin[i] = Math.min(nums[i], Math.min(nums[i] * dpMin[i - 1], nums[i] * dpMax[i - 1]));
+            res = Math.max(res, dpMax[i]);
         }
-        return max;
+        return res;
     }
 }
 // @lc code=end
