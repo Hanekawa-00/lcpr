4dfa95813d4f1510399440957bf8c42c6747b905
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-04-26 13:24:43 +0800

    打家劫舍

--- 198.house-robber.java
+++ 198.house-robber.java
@@ -5,37 +5,34 @@
  * [198] 打家劫舍
  */
 
-
 // @lcpr-template-start
 
 // @lcpr-template-end
 // @lc code=start
 class Solution {
-    public int rob(int[] nums) {
-        // 这里的pre为dp[n-1]，也就是前面的最大可得金额
-        int pre  = 0;
-        // 本次最大金额dp[n]，可能是打过上家，不能打此家，可能是打过上上家，要打此家
-        int curr =0;
-      for (int num : nums) {
-        int temp = curr;
-        curr = Math.max(pre+num,curr);
-        pre  = temp;
-      }
-        return curr;
+  public int rob(int[] nums) {
+    if (nums.length == 1) {
+      return nums[0];
     }
+    int[] dp = new int[nums.length];
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
+    for (int i = 2; i < dp.length; i++) {
+      // dp[i - 2] + nums[i] 表示不偷前一个房子？可是dp[i-1]也有机率不偷第i-1这个房子？
+      dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+    }
+    return dp[nums.length - 1];
+  }
 }
 // @lc code=end
 
-
-
 /*
-// @lcpr case=start
-// [1,2,3,1]\n
-// @lcpr case=end
-
-// @lcpr case=start
-// [2,7,9,3,1]\n
-// @lcpr case=end
-
+ * // @lcpr case=start
+ * // [1,2,3,1]\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // [2,7,9,3,1]\n
+ * // @lcpr case=end
+ * 
  */
-
