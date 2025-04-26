54a4199c8fa22d9e8fa5b4462692e4740e54c2bf
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-04-26 13:24:27 +0800

    轮转数组

--- 189.rotate-array.java
+++ 189.rotate-array.java
@@ -5,7 +5,6 @@
  * [189] 轮转数组
  */
 
-
 // @lcpr-template-start
 
 // @lcpr-template-end
@@ -13,25 +12,29 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
-        int[] newArr = new int[n];
-        for (int i = 0; i < n; ++i) {
-            newArr[(i + k) % n] = nums[i];
+        int[] tempArr = new int[n];
+        // 控制k的范围，因为k有可能大于数组长度
+        k = k % n;
+        if (k == 0) {
+            return;
         }
-        System.arraycopy(newArr, 0, nums, 0, n);
+        for (int i = 0; i < tempArr.length; i++) {
+            tempArr[i] = nums[(n - k + i) % n]; // 因为是右移，所以要使用n-k作为偏差值
+        }
+        for (int i = 0; i < n; i++) {
+            nums[i] = tempArr[i];
+        }
     }
 }
 // @lc code=end
 
-
-
 /*
-// @lcpr case=start
-// [1,2,3,4,5,6,7]\n3\n
-// @lcpr case=end
-
-// @lcpr case=start
-// [-1,-100,3,99]\n2\n
-// @lcpr case=end
-
+ * // @lcpr case=start
+ * // [1,2,3,4,5,6,7]\n3\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // [-1,-100,3,99]\n2\n
+ * // @lcpr case=end
+ * 
  */
-
