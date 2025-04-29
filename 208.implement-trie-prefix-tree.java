3df65879f6ad687144e70ae156ba47310d442ec7
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-04-29 21:44:33 +0800

    前缀树

--- 208.implement-trie-prefix-tree.java
+++ 208.implement-trie-prefix-tree.java
@@ -11,47 +11,56 @@
 // @lc code=start
 
 class Trie {
-    private Trie[] children;
-    private boolean isEnd;
+    private class TrieNode {
+        TrieNode[] children;
+
+        boolean isEndOfWord;
+
+        public TrieNode() {
+            children = new TrieNode[26];
+            isEndOfWord = false;
+        }
+    }
+
+    private final TrieNode root;
 
     public Trie() {
-        children = new Trie[26];
-        isEnd = false;
+        root = new TrieNode();
     }
 
     public void insert(String word) {
-        Trie node = this;
-        for (int i = 0; i < word.length(); i++) {
-            char ch = word.charAt(i);
+        TrieNode currNode = root;
+        // 构建树
+        for (char ch : word.toCharArray()) {
             int index = ch - 'a';
-            if (node.children[index] == null) {
-                node.children[index] = new Trie();
+            if (currNode.children[index] == null) { // 为空则创建新节点，否则复用
+                currNode.children[index] = new TrieNode();
             }
-            node = node.children[index];
+            currNode = currNode.children[index];
         }
-        node.isEnd = true;
+        currNode.isEndOfWord = true;// isEndOfWord可以完美解决重复路径问题，从根节点到这个节点有且只有一条路径
     }
 
     public boolean search(String word) {
-        Trie node = searchPrefix(word);
-        return node != null && node.isEnd;
-    }
-
-    private Trie searchPrefix(String prefix) {
-        Trie node = this;
-        for (int i = 0; i < prefix.length(); i++) {
-            char ch = prefix.charAt(i);
-            int index = ch - 'a';
-            if (node.children[index] == null) {
-                return null;
-            }
-            node = node.children[index];
-        }
-        return node;
+        TrieNode node = searchPrefix(word);
+        return node != null && node.isEndOfWord;
     }
 
     public boolean startsWith(String prefix) {
-        return searchPrefix(prefix) != null;
+        TrieNode node = searchPrefix(prefix);
+        return node != null;
+    }
+
+    public TrieNode searchPrefix(String str) {
+        TrieNode currNode = root;
+        for (char ch : str.toCharArray()) {
+            int index = ch - 'a';
+            if (currNode.children[index] == null) { // 如果为null则说明没有被insert
+                return null;
+            }
+            currNode = currNode.children[index];// 深度遍历
+        }
+        return currNode;// 便利完成说明找到完整目标str
     }
 }
 
