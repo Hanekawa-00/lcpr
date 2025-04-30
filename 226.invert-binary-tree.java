00676c8f742c41e02fc8af3b9007163ce6a48865
zzh <zzh04004@gmail.com>
2025-04-30 07:04:02 +0000

    反转二叉树

--- 226.invert-binary-tree.java
+++ 226.invert-binary-tree.java
@@ -27,12 +27,12 @@
 class Solution {
     public TreeNode invertTree(TreeNode root) {
         if (root == null) {
-            return root;
+            return null;
         }
         TreeNode left = root.left;
         TreeNode right = root.right;
-        root.left = invertTree(right);
         root.right = invertTree(left);
+        root.left = invertTree(right);
         return root;
     }
 }
