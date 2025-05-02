1a8fde397e2b76ab4bb61d9b192b9202c3d0ea09
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-05-02 18:27:08 +0800

    完全平方数

--- 279.perfect-squares.java
+++ 279.perfect-squares.java
@@ -9,20 +9,19 @@
 
 // @lcpr-template-end
 // @lc code=start
+
+import java.util.Arrays;
+
 class Solution {
     public int numSquares(int n) {
-        // 创建dp，用来维护组成数字i的最小完成平方数数量
+        // dp[i] 维护组成i所需要的最少的完全平方数
         int[] dp = new int[n + 1];
-        // 初始化dp,01
+        // 初始化
+        Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
-        dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            // 初始化i中的数值，最坏情况为i个1组成的
-            dp[i] = i;
-            // 一个一个进行尝试
+        for (int i = 1; i <= n; i++) {
+            // 假设j*j是组成i的最后一个完全平方数
             for (int j = 1; j * j <= i; j++) {
-                // 这里的dp[i - j * j]肯定比i小，所以成立
-                // 与上一个值作对比，取小的
                 dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
             }
         }
