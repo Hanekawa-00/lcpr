80b4924b34ae3d6cc5f32999acc8aedd61795e4c
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-05-03 12:25:57 +0800

    最长递增子序列

--- 300.longest-increasing-subsequence.java
+++ 300.longest-increasing-subsequence.java
@@ -10,28 +10,23 @@
 // @lcpr-template-end
 // @lc code=start
 
+import java.util.Arrays;
+
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        int n = nums.length;
-        // dp[i]用来表示以nums[i]作为结尾的子序列的最大长度
-        int[] dp = new int[n];
-        int maxLength = 1;
-        for (int i = 0; i < n; i++) {
-            dp[i] = 1;
+        // dp[i]维护以nums[i]结尾的最常递增子序列长度
+        int[] dp = new int[nums.length];
+        int res = 1;
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < dp.length; i++) {
             for (int j = 0; j < i; j++) {
-                // 如果nums[i]比nums[j]大，也就是第i个元素之前的一个元素比它小
-                // 注意这里是直接加上以j为结尾的子序列，也就是说这里不用担心这其中是否是有序
-                if (nums[i] > nums[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                if (nums[j] < nums[i]) {// 如果j位置上的数小于i位置上的数，说明i可以拼接到以j为结尾的子序列后面
+                    dp[i] = Math.max(dp[i], dp[j] + 1);// 不断更新取最大值
                 }
             }
-            // 更新maxLength
-            maxLength = Math.max(maxLength, dp[i]);
+            res = Math.max(dp[i], res);
         }
-        return maxLength;
+        return res;
     }
 }
 // @lc code=end
