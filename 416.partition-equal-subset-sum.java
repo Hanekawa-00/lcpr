43f025ad0a92469c9efe9cb4705de769bf0498c5
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-05-05 19:06:27 +0800

    分割等和子集

--- 416.partition-equal-subset-sum.java
+++ 416.partition-equal-subset-sum.java
@@ -11,22 +11,25 @@
 // @lc code=start
 class Solution {
     public boolean canPartition(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return false;
+        }
         int sum = 0;
         for (int num : nums) {
             sum += num;
         }
-        // 如果和为奇数，说明不可能分割为两个和相同的子数组
         if (sum % 2 != 0) {
             return false;
         }
         int target = sum / 2;
-        // dp[i]用来维护从nums数组取出一些值，这些值的和正好为i是否为真
+        // dp[i] 维护是否能凑成i的两个子集
         boolean[] dp = new boolean[target + 1];
-        dp[0] = true;// 和为0则一定能够实现
+        dp[0] = true;
         for (int num : nums) {
-            for (int j = target; j >= num; j--) {
-                // 选择num或者不选择num
-                dp[j] = dp[j] || dp[j - num];
+            for (int i = target; i >= num; i--) {
+                // 选择num或者不选
+                dp[i] = dp[i] || dp[i - num];
             }
         }
         return dp[target];
