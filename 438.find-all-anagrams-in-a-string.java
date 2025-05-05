f3a92e6093ce06b72d69deab007a433cd690d7e5
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-05-05 19:07:24 +0800

    所有字母异位词

--- 438.find-all-anagrams-in-a-string.java
+++ 438.find-all-anagrams-in-a-string.java
@@ -11,42 +11,54 @@
 // @lc code=start
 
 import java.util.ArrayList;
+import java.util.Arrays; // 需要导入 Arrays 用于比较数组
 import java.util.List;
+// 移除了未使用的 Map 导入
 
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> ans = new ArrayList<>();
-        if (s.length() < p.length())
-            return ans;
+        List<Integer> res = new ArrayList<>();
+        int sLen = s.length();
+        int pLen = p.length();
 
-        int[] map = new int[26];
-        for (char ch : p.toCharArray()) {
-            map[ch - 'a']++;
+        // 如果 p 比 s 长，不可能找到异位词
+        if (pLen > sLen) {
+            return res;
         }
 
-        int left = 0, right = 0, count = p.length();
-        while (right < s.length()) {
-            if (map[s.charAt(right) - 'a'] > 0) {
-                count--;
-            }
-            map[s.charAt(right) - 'a']--;
-            right++;
+        // 1. 创建频率数组
+        int[] pFreq = new int[26]; // 存储 p 中字符的频率
+        int[] windowFreq = new int[26]; // 存储当前窗口中字符的频率
 
-            if (count == 0) {
-                ans.add(left);
-            }
+        // 2. 计算 p 的频率 和 初始化第一个窗口的频率
+        for (int i = 0; i < pLen; i++) {
+            pFreq[p.charAt(i) - 'a']++;
+            windowFreq[s.charAt(i) - 'a']++;
+        }
 
-            if (right - left == p.length()) {
-                if (map[s.charAt(left) - 'a'] >= 0) {
-                    count++;
-                }
-                map[s.charAt(left) - 'a']++;
-                left++;
+        // 3. 检查第一个窗口是否是异位词
+        if (Arrays.equals(pFreq, windowFreq)) {
+            res.add(0);
+        }
+
+        // 4. 滑动窗口
+        // left 指向窗口的起始位置，right 指向窗口的结束位置的下一个位置
+        // 窗口范围是 [left, right - 1]
+        for (int i = pLen; i < sLen; i++) {
+            // 窗口向右滑动：
+            // 移出窗口的字符 (最左边的字符)
+            windowFreq[s.charAt(i - pLen) - 'a']--;
+            // 加入窗口的字符 (新进入的最右边的字符)
+            windowFreq[s.charAt(i) - 'a']++;
+
+            // 5. 检查当前窗口是否是异位词
+            if (Arrays.equals(pFreq, windowFreq)) {
+                // 如果是，添加窗口的起始索引 (i - pLen + 1)
+                res.add(i - pLen + 1);
             }
         }
-        return ans;
+        return res;
     }
-
 }
 
 // @lc code=end
@@ -55,9 +67,9 @@ class Solution {
  * // @lcpr case=start
  * // "cbaebabacd"\n"abc"\n
  * // @lcpr case=end
- * 
+ *
  * // @lcpr case=start
  * // "abab"\n"ab"\n
  * // @lcpr case=end
- * 
+ *
  */
