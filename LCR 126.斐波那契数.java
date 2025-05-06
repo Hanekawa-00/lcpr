5f2afb84c87be2be70207ac35c77f3a2e813eb97
zzh <zzh04004@gmail.com>
2025-05-06 10:01:56 +0000

    斐波那契

--- LCR 126.斐波那契数.java
+++ LCR 126.斐波那契数.java
@@ -11,17 +11,17 @@
 // @lc code=start
 class Solution {
     public int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
         int[] dp = new int[n + 1];
         dp[0] = 0;
-        if (n == 0) {
-            return dp[0];
+        if (n == 1) {
+            return 1;
         }
         dp[1] = 1;
-        if (n == 1) {
-            return dp[1];
-        }
         for (int i = 2; i < dp.length; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+            dp[i] =( dp[i - 2] + dp[i - 1]) % 1000000007;
         }
         return dp[n];
     }
