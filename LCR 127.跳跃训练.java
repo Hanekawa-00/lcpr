7fc45f11f4df604c3338ed614efc3894882a0d4e
hanekawa <zzh04004@gmail.com>
2025-05-07 19:01:22 +0800

    优化火车路径计算方法，简化边界条件处理

--- LCR 127.跳跃训练.java
+++ LCR 127.跳跃训练.java
@@ -11,16 +11,13 @@
 // @lc code=start
 class Solution {
     public int trainWays(int num) {
+        if (num == 0) {
+            return 1;
+        }
         int[] dp = new int[num + 1];
         dp[0] = 1;
-        if (num == 0) {
-            return dp[0];
-        }
         dp[1] = 1;
-        if (num == 1) {
-            return dp[1];
-        }
-        for (int i = 2; i < dp.length; i++) {
+        for (int i = 2; i <= num; i++) {
             dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
         }
         return dp[num];
