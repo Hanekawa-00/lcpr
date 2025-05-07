2adfded2597ad66a1590b0b95f46ca70d7859a2f
hanekawa <zzh04004@gmail.com>
2025-05-07 20:16:00 +0800

    二分查找？？？

--- LCR 128.库存管理-i.java
+++ LCR 128.库存管理-i.java
@@ -16,10 +16,16 @@ class Solution {
         while (left < right) {
             int mid = (left + right) / 2;
             int curr = stock[mid];
-            if (curr <= stock[right]) {
+            if (curr < stock[right]) {
                 right = mid;
-            } else {
+            } else if (curr > stock[right]) {
                 left = mid + 1;
+            } else {
+                // 注意这里可能出现重复元素，即mid可能与right的值相同，但是最小值在他们之间
+                // 我们可以安全地将 right 减一。这是因为如果 stock[right] 是最小元素，那么 stock[mid] 也等于它，并且 mid 小于
+                // right (除非 left == mid == right)。将 right 减一并不会错过最小元素。如果 stock[right]
+                // 不是最小元素，将它排除掉也不会影响找到最小元素。
+                right--;
             }
         }
         return stock[left];
