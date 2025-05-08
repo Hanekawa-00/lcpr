53bb3a7e0251dc4ff8c28d0745ffb8cf5a7ee45e
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-05-08 18:41:01 +0800

    衣橱整理

--- LCR 130.衣橱整理.java
+++ LCR 130.衣橱整理.java
@@ -12,44 +12,26 @@
 class Solution {
     public int wardrobeFinishing(int m, int n, int cnt) {
         boolean[][] visited = new boolean[m][n];
-        return dfs(0, 0, m, n, cnt, visited);
+        return dfs(0, 0, m, n, visited, cnt);
     }
 
-    /**
-     * @param i       当前横坐标
-     * @param j       纵坐标
-     * @param m
-     * @param n
-     * @param cnt
-     * @param visited 维护每个格子的访问情况
-     * @return
-     */
-    private int dfs(int i, int j, int m, int n, int cnt, boolean[][] visited) {
-        // 边界检查
-        if (i < 0 || i >= m || j < 0 || j >= n) {
+    public int dfs(int i, int j, int m, int n, boolean[][] visited, int cnt) {
+        if (i >= m || j >= n || visited[i][j]) {
             return 0;
         }
-        // 如果已经访问或者不满足条件则退出（因为是返回int类型，所以在不影响结果的条件下使用0）
-        if (visited[i][j] || (digitSum(i) + digitSum(j)) > cnt) {
+        if (digitSum(i) + digitSum(j) > cnt) {
             return 0;
         }
-        // 标记为已访问
         visited[i][j] = true;
-        // 计数，并且向右边和下边继续搜索
-        return 1 + dfs(i + 1, j, m, n, cnt, visited) + dfs(i, j + 1, m, n, cnt, visited);
+        // 这里与回溯的区别就是这题指定出发点，而不需要一次次尝试
+        return 1 + dfs(i + 1, j, m, n, visited, cnt) + dfs(i, j + 1, m, n, visited, cnt);
     }
 
-    /**
-     * 计算各位数之和
-     * 
-     * @param x
-     * @return
-     */
-    private int digitSum(int x) {
+    private int digitSum(int num) {
         int sum = 0;
-        while (x > 0) {
-            sum += x % 10;
-            x /= 10;
+        while (num != 0) {
+            sum += num % 10; // 取个位数
+            num /= 10;
         }
         return sum;
     }
