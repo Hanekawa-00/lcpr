f39cf5a5269be6e92616d7c70441d1873643de02
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-05-11 19:10:49 +0800

    螺旋遍历二维数组

--- LCR 146.螺旋遍历二维数组.java
+++ LCR 146.螺旋遍历二维数组.java
@@ -11,45 +11,47 @@
 // @lc code=start
 class Solution {
     public int[] spiralArray(int[][] array) {
-        if (array == null || array.length == 0 || array[0].length == 0) {
+        if (array.length == 0 || array[0].length == 0) {
             return new int[0];
         }
-        int rows = array.length;
-        int cols = array[0].length;
-        int[] res = new int[rows * cols];
-        int index = 0;
-        int top = 0, bottom = rows - 1, left = 0, right = cols - 1;
-        while (top <= bottom && left <= right) {
-            // 遍历最上层
+        int m = array.length;
+        int n = array[0].length;
+        int[] res = new int[m * n];
+        // 维护边界值,在遍历的过程中逐步缩小边界[上下左右边界]
+        int left = 0;
+        int right = n - 1;
+        int top = 0;
+        int bottom = m - 1;
+        int x = 0;
+        while (left <= right && top <= bottom) {
             for (int i = left; i <= right; i++) {
-                res[index] = array[top][i];
-                index++;
+                res[x++] = array[top][i];
             }
-            // 收缩
             top++;
+            if (top > bottom)
+                break;
             for (int i = top; i <= bottom; i++) {
-                res[index] = array[i][right];
-                index++;
+                res[x++] = array[i][right];
             }
             right--;
-            // 检查是否剩余行，如果之前遍历最上层已经是最后一层，那么top++之后已经超过了bottom
-            if (top <= bottom) {
-                for (int i = right; i >= left; i--) {
-                    res[index] = array[bottom][i];
-                    index++;
-                }
-                bottom--;
+            if (right < left)
+                break;
+            for (int i = right; i >= left; i--) {
+                res[x++] = array[bottom][i];
             }
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    res[index] = array[i][left];
-                    index++;
-                }
-                left++;
+            bottom--;
+            if (bottom < top)
+                break;
+            for (int i = bottom; i >= top; i--) {
+                res[x++] = array[i][left];
             }
+            left++;
+            // if (left > right)
+            //     break;
         }
         return res;
     }
+
 }
 
 // @lc code=end
