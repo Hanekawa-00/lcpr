d3f4e887ffb500e9f999b575ec19378eb979ea40
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-05-14 13:49:36 +0800

    二叉搜索树转化为排序双向链表

--- LCR 155.将二叉搜索树转化为排序的双向链表.java
+++ LCR 155.将二叉搜索树转化为排序的双向链表.java
@@ -30,44 +30,37 @@ class Node {
 };
 */
 class Solution {
-    // 在中序遍历时记住最左节点以及前一个节点
-    // 用来维护链表头
-    Node head = null;
-    // 上一个节点
-    Node prev = null;
+    // 只保存头节点
+    private Node head;
+    // 这个节点用来中序遍历
+    private Node node;
 
     public Node treeToDoublyList(Node root) {
         if (root == null) {
-            return null;
+            return root;
         }
-        // 中序遍历
         dfs(root);
-        // head当前为第一个节点
-        head.left = prev;
-        // 此时prev已经到最后一个节点了
-        prev.right = head;
+        head.left = node;
+        node.right = head;
         return head;
     }
 
-    private void dfs(Node curr) {
-        if (curr == null) {
+    private void dfs(Node root) {
+        if (root == null) {
             return;
         }
-        // 遍历左子树
-        dfs(curr.left);
-        // 处理当前子树根结点
-        if (prev == null) {
-            // 如果prev为空说明这是第一个元素
-            head = curr;
+        dfs(root.left);
+        //
+        if (node == null) {
+            head = root;
+            node = root;
         } else {
-            // 非第一个元素，将上一个元素的right（也就是next）指向当前元素
-            prev.right = curr;
-            // 当前元素的left（也就是pre）指向前一个元素
-            curr.left = prev;
+            node.right = root;
+            root.left = node;
+            node = node.right;// 更新
         }
-        // 准备递归右子树，更新prev为当前节点
-        prev = curr;
-        dfs(curr.right);
+        //
+        dfs(root.right);
     }
 }
 // @lc code=end
