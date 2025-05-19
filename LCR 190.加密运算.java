8d8dfd764eab5c87c8c19e93a83e9f4f5edbfb0d
zzh <zzh04004@gmail.com>
2025-05-19 03:53:18 +0000

    使用位运算来实现加法pass

--- LCR 190.加密运算.java
+++ LCR 190.加密运算.java
@@ -5,37 +5,46 @@
  * [LCR 190] 加密运算
  */
 
-
 // @lcpr-template-start
 
 // @lcpr-template-end
 // @lc code=start
 class Solution {
     public int encryptionCalculate(int dataA, int dataB) {
-          // 不使用四则运算实现两数相加
-          while (dataB != 0) {
-            // 计算无进位和
-            int sum = dataA ^ dataB;
-            
-            // 计算进位
-            int carry = (dataA & dataB) << 1;
-            
-            // 更新 dataA 和 dataB 用于下一轮计算
-            dataA = sum;
-            dataB = carry;
+        // 将 dataA 和 dataB 赋值给临时变量 a 和 b，方便后续迭代
+        // 在每次迭代中，a 存储当前的“无进位和”，b 存储当前的“进位”
+        int a = dataA;
+        int b = dataB;
+
+        // 当 b (相当于进位) 不为 0 时，循环进行
+        // 循环的目的是将进位不断地加到无进位和上，直到没有进位为止
+        while (b != 0) {
+            // 步骤 1: 计算进位 (carry)
+            // (a & b) 找出 a 和 b 中都为 1 的位，这些位会产生进位
+            // << 1 将进位左移一位，放到下一位进行处理
+            int carry = (a & b) << 1;
+
+            // 步骤 2: 计算当前位的和 (sum without carry)
+            // a ^ b 异或操作，实现无进位加法
+            // 结果存储在 a 中，作为下一轮的“无进位和”
+            a = a ^ b;
+
+            // 步骤 3: 更新 b 为当前的进位
+            // 将计算出的进位赋值给 b，作为下一轮循环的加数
+            // 下一轮循环实际上是计算 (a ^ b) + ((a & b) << 1)
+            b = carry;
         }
-        
-        return dataA;
+
+        // 当 b (进位) 为 0 时，循环结束
+        // 此时 a 中存储的就是最终的无进位和，也就是最终的和
+        return a;
     }
 }
 // @lc code=end
 
-
-
 /*
-// @lcpr case=start
-// 5\n-1\n
-// @lcpr case=end
-
+ * // @lcpr case=start
+ * // 5\n-1\n
+ * // @lcpr case=end
+ * 
  */
-
