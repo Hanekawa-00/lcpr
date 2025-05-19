ec9f65bd682a234b08f96b1ba698835d74ea1bbb
zzh <zzh04004@gmail.com>
2025-05-19 03:52:53 +0000

    丑数

--- LCR 168.丑数.java
+++ LCR 168.丑数.java
@@ -11,38 +11,37 @@
 // @lc code=start
 class Solution {
     /**
-     * 任何一个丑数都可以由一个较小的丑数乘以2、3或5得到。
+     * 丑数序列是递增的，并且任何一个丑数都是由前面的某个丑数乘以 2、3 或 5 得到的
      * 
      * @param n
      * @return
      */
     public int nthUglyNumber(int n) {
-        // 定义dp数组，dp[i]表示第i个丑数
-        int[] dp = new int[n + 1];
-        dp[1] = 1; // 第一个丑数是1
-
-        // 定义三个指针，分别指向下一个要乘以2、3、5的丑数的位置
-        int p2 = 1, p3 = 1, p5 = 1;
-
-        for (int i = 2; i <= n; i++) {
-            // 计算下一个丑数的候选值
-            int num2 = dp[p2] * 2;
-            int num3 = dp[p3] * 3;
-            int num5 = dp[p5] * 5;
-
-            // 取最小值作为下一个丑数
-            dp[i] = Math.min(Math.min(num2, num3), num5);
-
-            // 更新指针（注意：可能有多个指针同时移动，比如当6既是2*3也是3*2时）
-            if (dp[i] == num2)
-                p2++;
-            if (dp[i] == num3)
+        int[] dp = new int[n];
+        // 初始化第一个丑数
+        dp[0] = 1;
+        // 初始化指针，因为此时只有一个丑数
+        int p2 = 0;
+        int p3 = 0;
+        int p5 = 0;
+        for (int i = 1; i < dp.length; i++) {
+            int next2 = dp[p2] * 2;
+            int next3 = dp[p3] * 3;
+            int next5 = dp[p5] * 5;
+            // 因为是有序排列，所以要取最小的排列
+            dp[i] = Math.min(next2, Math.min(next3, next5));
+            // 因为可能出现相同的情况，所以if要分开写
+            if (next2 == dp[i]) {
+                p2++;// 说明当前丑数已经被使用，指针后移
+            }
+            if (next3 == dp[i]) {
                 p3++;
-            if (dp[i] == num5)
+            }
+            if (next5 == dp[i]) {
                 p5++;
+            }
         }
-
-        return dp[n];
+        return dp[n-1];
     }
 }
 // @lc code=end
