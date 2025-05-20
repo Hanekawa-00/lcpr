dc684704bb6d4fe77ba973a12862c378ab824f16
zzh <zzh04004@gmail.com>
2025-05-20 04:31:21 +0000

    左右两边乘积

--- LCR 191.按规则计算统计结果.java
+++ LCR 191.按规则计算统计结果.java
@@ -11,26 +11,25 @@
 // @lc code=start
 class Solution {
     public int[] statisticalResult(int[] arrayA) {
+        if (arrayA == null || arrayA.length == 0) {
+            return new int[0];
+        }
         int n = arrayA.length;
-        if (n == 0) {
-            return new int[] {};
+        int[] leftDp = new int[n];
+        leftDp[0] = 1;
+        int[] rightDp = new int[n];
+        rightDp[n - 1] = 1;
+        for (int i = 1; i < n; i++) {
+            leftDp[i] = leftDp[i - 1] * arrayA[i - 1];
         }
-        int[] arrayB = new int[n];
-        // 计算每个数字左边的乘积（从左向右遍历，不受右边的数影响）
-        int leftProduct = 1;
-        for (int i = 0; i < n; i++) {
-            arrayB[i] = leftProduct;
-            // 更新左边乘积
-            leftProduct *= arrayA[i];
+        for (int i = n - 2; i >= 0; i--) {
+            rightDp[i] = rightDp[i + 1] * arrayA[i + 1];
         }
-        // 计算每个数的右边乘积并和原来的左边乘积相乘
-        int rightProduct = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            // 这里其实是两步骤，先计算右边乘积，再与左边乘积相乘
-            arrayB[i] *= rightProduct;
-            rightProduct *= arrayA[i];
+        int[] res = new int[n];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = leftDp[i] * rightDp[i];
         }
-        return arrayB;
+        return res;
     }
 }
 // @lc code=end
