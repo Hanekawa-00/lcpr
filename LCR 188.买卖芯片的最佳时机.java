b356d11817d5a75681fe196f43578cf2a424aa8b
zzh <zzh04004@gmail.com>
2025-05-21 03:31:14 +0000

    买卖最佳时机

--- LCR 188.买卖芯片的最佳时机.java
+++ LCR 188.买卖芯片的最佳时机.java
@@ -15,27 +15,16 @@ class Solution {
             return 0;
         }
         int len = prices.length;
-        // minPrice用来维护第i天前最小购入价格
-        int[] minPrice = new int[len + 1];
-        // 初始化第一天前购入价格（理论上不可能）
-        minPrice[1] = Integer.MAX_VALUE;
-        // 初始化前一天的价格，比较的基准
-        int pre = prices[0];
-        for (int i = 2; i < minPrice.length; i++) {
-            // 向后遍历不断更新
-            minPrice[i] = Math.min(pre, minPrice[i - 1]);
-            pre = prices[i - 1];
+        // dp[i]维护第i天可以获得最大利润
+        int[] dp = new int[len];
+        dp[0] = 0;
+        int min = prices[0];
+        for (int i = 1; i < dp.length; i++) {
+            // 这天按照昨天的计划或者卖出
+            dp[i] = Math.max(dp[i - 1], prices[i] - min);
+            min = Math.min(min, prices[i]);
         }
-        // dp[i]用来表示前i天最大利润
-        int[] dp = new int[len + 1];
-        dp[1] = 0;
-        for (int i = 2; i < dp.length; i++) {
-            int curr = prices[i - 1];
-            // 这天之前的最小购入价
-            int min = minPrice[i];
-            dp[i] = Math.max(dp[i - 1], curr - min);
-        }
-        return dp[len];
+        return dp[len - 1];
     }
 }
 // @lc code=end
