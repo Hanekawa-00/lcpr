8d5942f697f3903a2a5a961a4fee15239c6adf0d
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-05-26 10:07:06 +0800

    缺失的第一个正数pass

--- 41.first-missing-positive.java
+++ 41.first-missing-positive.java
@@ -14,28 +14,34 @@ import java.util.Arrays;
 
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        Arrays.sort(nums);
-        int pre = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) {
-                pre = i;
-                break;
+        int n = nums.length;
+
+        // 阶段一：原地放置数字
+        // 目标：将数字 x 放到索引 x-1 的位置
+        for (int i = 0; i < n; i++) {
+            // 当 nums[i] 是一个有效的正数 (在 1 到 n 之间)
+            // 并且 nums[i] 不在它应该在的位置 (nums[i] - 1 != i)
+            // 并且 nums[i] 与它目标位置上的数不同 (避免与重复数字无限交换)
+            // (nums[nums[i] - 1] != nums[i])
+            while (nums[i] >= 1 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                // 交换 nums[i] 和 nums[nums[i]-1]
+                int targetIndex = nums[i] - 1;
+                int temp = nums[targetIndex];
+                nums[targetIndex] = nums[i];
+                nums[i] = temp;
             }
         }
-        // 第一个大于0的数不存在或者大于1，则直接返回1
-        if (nums[pre] > 1 || nums[pre] < 0) {
-            return 1;
-        }
-        for (int i = pre + 1; i < nums.length; i++) {
-            // 其中可能有重复
-            if (nums[i] == nums[pre] + 1 || nums[i] == nums[pre]) {
-                pre = i;
-                continue;
-            } else {
-                return nums[pre] + 1;
+
+        // 阶段二：查找第一个缺失的正数
+        // 遍历数组，找到第一个 nums[i] != i + 1 的位置
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1; // i+1 是缺失的最小正数
             }
         }
-        return nums[pre] + 1;
+
+        // 如果所有 1 到 n 的数字都存在，则缺失的最小正数是 n + 1
+        return n + 1;
     }
 }
 // @lc code=end
