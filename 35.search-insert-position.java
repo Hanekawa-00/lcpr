25d2b9a6296cf5a24e698de50e5a1e24fa5dafaf
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-05-26 10:06:12 +0800

    查找插入位置

--- 35.search-insert-position.java
+++ 35.search-insert-position.java
@@ -11,20 +11,17 @@
 // @lc code=start
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        while (left <= right) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
             int mid = (left + right) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            }
             if (nums[mid] < target) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
-        return left;
+        // 放在当前位置或者当前位置的后面
+        return nums[left] < target ? left + 1 : left;
     }
 }
 // @lc code=end
