d557aea592a99b3ad07e0d9ff1f5227eb1ffe990
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-05-28 12:59:13 +0800

    跳跃游戏

--- 55.jump-game.java
+++ 55.jump-game.java
@@ -10,32 +10,19 @@
 // @lcpr-template-end
 // @lc code=start
 class Solution {
-    /**
-     * 跳跃游戏
-     * 
-     * @param nums
-     * @return
-     */
+
     public boolean canJump(int[] nums) {
-        if (nums.length == 1) {
-            return true;
-        }
-        // dp[i]用来维护跳跃i步数所能达到的最远距离
-        int[] dp = new int[nums.length];
-        dp[0] = 0;
-        dp[1] = nums[0];
-        if (dp[1] >= nums.length - 1) {
-            return true;
-        }
-        for (int i = 2; i < dp.length; i++) {
-            int maxReach = dp[i - 1];
-            for (int j = dp[i - 2]; j <= dp[i - 1]; j++) {
-                maxReach = Math.max(maxReach, nums[j] + j);
+        int maxReach = 0;
+        // 维护从上一个maxReach到当前maxReach的区间内所能跳跃到的最远索引
+        int curr = 0;
+        for (int i = 0; i < nums.length; i++) {
+            curr = Math.max(curr, i + nums[i]);
+            if (i == maxReach) {
+                maxReach = curr;
+                if (maxReach >= nums.length - 1) {
+                    return true;
+                }
             }
-            if (maxReach >= nums.length - 1) {
-                return true;
-            }
-            dp[i] = maxReach;
         }
         return false;
     }
