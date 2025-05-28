6f8c0c11ebf4c127be4f6b65a494015bd5885012
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-05-28 12:59:46 +0800

    最小路径和

--- 64.minimum-path-sum.java
+++ 64.minimum-path-sum.java
@@ -12,21 +12,19 @@
 
 class Solution {
     public int minPathSum(int[][] grid) {
-        int m = grid.length;// 行数
-        int n = grid[0].length; // 列数
-        // dp[i][j]表示到[i,j]位置的最小路径和
+        int m = grid.length;
+        int n = grid[0].length;
         int[][] dp = new int[m][n];
         dp[0][0] = grid[0][0];
-        // 初始化第一行第一列
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
         for (int i = 1; i < n; i++) {
             dp[0][i] = dp[0][i - 1] + grid[0][i];
         }
         for (int i = 1; i < m; i++) {
-            dp[i][0] = dp[i - 1][0] + grid[i][0];
-        }
-        for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
             }
         }
         return dp[m - 1][n - 1];
