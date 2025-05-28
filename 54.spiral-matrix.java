ed4eacfc8051370c9209364937cb3a63a9777603
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-05-28 12:59:04 +0800

    螺旋矩阵遍历

--- 54.spiral-matrix.java
+++ 54.spiral-matrix.java
@@ -15,41 +15,30 @@ import java.util.List;
 
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        if (matrix.length == 0 || matrix[0].length == 0) {
-            return new ArrayList<>();
-        }
-        // 初始化上下左右边界值
-        int top = 0;
-        int bottom = matrix.length - 1;
-        int left = 0;
-        int right = matrix[0].length - 1;
         List<Integer> resList = new ArrayList<>();
-        // 螺旋遍历一直缩小边界，直至重合
+        int rows = matrix.length;
+        int cals = matrix[0].length;
+        int top = 0;
+        int bottom = rows - 1;
+        int left = 0;
+        int right = cals - 1;
         while (top <= bottom && left <= right) {
-            // top行
             for (int i = left; i <= right; i++) {
                 resList.add(matrix[top][i]);
             }
-            // 更新top
             top++;
-            if (top <= bottom && left <= right) {// 这里的=很关键，top是刚刚更新的
-                // right列
-                for (int i = top; i <= bottom; i++) {// 因为这里top已经++了，所以直接从top开始
-                    resList.add(matrix[i][right]);
-                }
-                right--;
+            for (int i = top; i <= bottom; i++) {
+                resList.add(matrix[i][right]);
             }
-            // 检查bottom行是否还有数字
-            if (top <= bottom && left <= right) {
-                // bottom行
+            right--;
+            // 防止越界重复添加，添加if判断 top++
+            if (top <= bottom) {
                 for (int i = right; i >= left; i--) {
                     resList.add(matrix[bottom][i]);
                 }
                 bottom--;
             }
-            // 检查是left列是否还有数字
-            if (top <= bottom && left <= right) {
-                // left列
+            if (left <= right) {
                 for (int i = bottom; i >= top; i--) {
                     resList.add(matrix[i][left]);
                 }
