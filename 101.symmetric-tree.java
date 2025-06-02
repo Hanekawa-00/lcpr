acb43ce164362540d7558394e1e7e1bacbe69e2f
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-06-02 16:13:37 +0800

    对称二叉树

--- 101.symmetric-tree.java
+++ 101.symmetric-tree.java
@@ -9,6 +9,7 @@
 
 // @lcpr-template-end
 // @lc code=start
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -26,22 +27,17 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if (root == null) {
-            return true;
-        }
-        return check(root.left, root.right);
+        return compare(root.left, root.right);
     }
 
-    private boolean check(TreeNode node1, TreeNode node2) {
+    private boolean compare(TreeNode node1, TreeNode node2) {
         if (node1 == null && node2 == null) {
             return true;
         }
         if (node1 == null || node2 == null) {
             return false;
         }
-        return node1.val == node2.val
-                && check(node1.left, node2.right)
-                && check(node1.right, node2.left);
+        return node1.val == node2.val && compare(node1.left, node2.right) && compare(node1.right, node2.left);
     }
 
 }
