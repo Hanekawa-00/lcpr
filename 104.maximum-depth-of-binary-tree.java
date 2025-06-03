93646f17c8d045a869ccbc254d1cfada6890424c
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-06-03 16:36:29 +0800

    二叉树最大深度

--- 104.maximum-depth-of-binary-tree.java
+++ 104.maximum-depth-of-binary-tree.java
@@ -30,9 +30,7 @@ class Solution {
         if (root == null) {
             return 0;
         }
-        int leftDepth = maxDepth(root.left);
-        int rightDepth = maxDepth(root.right);
-        return Math.max(leftDepth, rightDepth) + 1;
+        return  1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
 
 }
