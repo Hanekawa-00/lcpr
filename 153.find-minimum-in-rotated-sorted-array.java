1ace6f4f5aa4e58bb033e175e16d4c36f1930496
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-06-08 16:48:52 +0800

    发现旋转排序数组最小值

--- 153.find-minimum-in-rotated-sorted-array.java
+++ 153.find-minimum-in-rotated-sorted-array.java
@@ -5,19 +5,21 @@
  * [153] 寻找旋转排序数组中的最小值
  */
 
+// @lcpr-template-start
+
+// @lcpr-template-end
 // @lc code=start
 class Solution {
     public int findMin(int[] nums) {
-        int left = 0, right = nums.length - 1;
+        int left = 0;
+        int right = nums.length - 1;
         while (left < right) {
             int mid = (left + right) / 2;
-            // 要找最小值并缩小区间，必须与right对比
-            // 如果与left作对比，则只能说明[left,mid]是升序的，即使最小值在这个区间也同样成立，所以会导致误判
-            if (nums[mid] > nums[right]) {
-                left = mid + 1;
-            } else {
-                // 如果中间这个值小于right，则不能排除这个mid是目标值的可能性
+            // 尽力将范围维持在右边的递增区间内
+            if (nums[mid] <= nums[right]) {
                 right = mid;
+            }else{
+                left = mid + 1;
             }
         }
         return nums[left];
