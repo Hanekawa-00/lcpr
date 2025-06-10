2658b5718531ec4c25d52ee064e4a50234486eb0
ZHANG ZIHENG <142872170+Hanekawa-00@users.noreply.github.com>
2025-06-10 09:52:32 +0800

    数组倒数第k个大的元素

--- 215.kth-largest-element-in-an-array.java
+++ 215.kth-largest-element-in-an-array.java
@@ -15,13 +15,7 @@ import java.util.Arrays;
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         Arrays.sort(nums);
-        for (int i = nums.length - 1; i >= 0; i--) {
-            k--;
-            if (k == 0) {
-                return nums[i];
-            }
-        }
-        return -1;
+        return nums[nums.length - k];
     }
 
 }
